b02a191033915cab33e857413e3258f166c9aba9
Ben Follis <bfollis@Bens-MacBook-Pro.local>
2016-07-09 22:12:40 +0100

    Added fauxmo config generator

--- setup.py
+++ setup.py
@@ -7,4 +7,4 @@ setup(name='ism_io',
       license="GPLv3",
       packages=['ism_io', 'ism_io.config', 'ism_io.web', 'ism_io.bit_drivers', 'ism_io.socket_drivers'],
       zip_safe=False,
-      scripts=['bin/ism_io_rest'])
+      scripts=['bin/ism_io_rest', 'bin/ism_io_fauxmo_config'])
